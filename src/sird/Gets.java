2fe4820cee2545c026b3bd29136b0b041fa8657e
GiotoxKayser <38656274+GiotoxKayser@users.noreply.github.com>
2018-11-29 15:53:07 -0200

    1.0
    -criados as classes principais

---
+++ src/sird/Gets.java
@@ -0,0 +1,16 @@
+package sird;
+
+public class Gets {
+
+    public String getPasta() {
+        return pasta;
+    }
+
+    public void setPasta(String pasta) {
+        this.pasta = pasta;
+    }
+    String pasta;
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/sird/Main.java
@@ -0,0 +1,49 @@
+package sird;
+
+import dicionario.Dicionario;
+import indexador.Indexador;
+import static java.lang.System.out;
+import recuperador.Recuperador;
+
+public class Main {
+    
+    public static void main(String[] args) {
+
+        String [] bd = {"doc1.txt","doc2.txt","doc3.txt",
+                        "doc4.txt","doc5.txt","doc6.txt"};
+        
+        String pasta = "C:\\Users\\gioto\\Desktop\\src\\doc\\";
+        //String pasta = "C:\\Users\\PROJETO\\Documents\\SIRD\\src\\doc\\" ;
+        String separador = " ";
+        
+        Dicionario dico = new Dicionario();
+        dico.montar(pasta, bd, separador);
+        dico.mostrar();
+        
+        Indexador idx = new Indexador(dico);
+        idx.indexa(pasta, bd, separador);  
+        
+        int [][] TF = idx.getTF();
+        out.println("\nTF");
+        idx.mostrarMatriz(TF);
+        double [] DF = idx.getDF();
+        out.print("\n\nDF");
+        idx.mostrarVetor(DF);
+        double [] IDF = idx.getIDF();
+        out.print("\n\nIDF");
+        idx.mostrarVetor(IDF);
+        double [][] TFIDF = idx.getTFIDF();
+        out.print("\n\nTFIDF");
+        idx.mostrarMatriz(TFIDF);
+        
+        // continuar com as funcionalidades para 
+        // encontrar o documento foco
+        String consulta = "aa cc";
+        
+        Recuperador r = new Recuperador(separador, dico, IDF);
+        double [] cTFIDF = r.tfidf(consulta);
+        out.print("\n\nTFIDF da consulta");
+        r.mostrarVetor(cTFIDF);  
+        
+    }
+}
